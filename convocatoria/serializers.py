a51326fcec234de5bdbecf4e2f8ef1dd65a472a5
Gabriel Quiroz <gabriel@mb.company>
2021-03-02 09:48:06 -0600

    DONE-> GET precio a pagar-> CMCPER-64

--- convocatoria/serializers.py
+++ convocatoria/serializers.py
@@ -245,3 +245,20 @@ class ConvocatoriaEnroladoEngargoladoRechazarSerializer(serializers.ModelSeriali
     class Meta:
         model = ConvocatoriaEnroladoDocumento
         fields = ['id', 'engargoladoOk', 'notasEngargolado', 'rechazoEngargolado']
+
+
+class ConvocatoriaEnroladoMedicoAPagarDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ConvocatoriaEnrolado
+        fields = ['id', 'medico', 'convocatoria', 'catSedes', 'catTiposExamen']
+
+    def to_representation(self, instance):
+        repr = super().to_representation(instance)
+        repr['catTiposExamen'] = instance.catTiposExamen.descripcion
+        repr['catSedes'] = instance.catSedes.descripcion
+        repr['convocatoria'] = instance.convocatoria.nombre
+        repr['medico'] = instance.medico.nombre + ' ' + instance.medico.apPaterno
+        repr['aPagar'] = instance.catTiposExamen.precio
+        if instance.medico.estudioExtranjero:
+            repr['aPagar'] = instance.catTiposExamen.precioExtrangero
+        return repr
