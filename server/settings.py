3fffbe3a9e2086c9dcd542ea478c2a2d6973d03d
Gabriel Quiroz <gabriel@mb.company>
2021-07-05 17:06:28 -0500

    UPDATED-> jugando con logger v2

--- server/settings.py
+++ server/settings.py
@@ -257,3 +257,61 @@ EMAIL_PORT = config('EMAIL_PORT', default=465)
 #         },
 #     },
 # }
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'formatters': {
+        'django.server': {
+            '()': 'django.utils.log.ServerFormatter',
+            'format': '[{server_time}] {message}',
+            'style': '{',
+        },
+        'personal': {
+            'format': '{asctime}s - {levelname}s [{pathname}s:{lineno}s]-> \n {message}s',
+            'style': '{',
+        }        
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
+        # 'django.server': {
+        #     'level': 'INFO',
+        #     'class': 'logging.StreamHandler',
+        #     'formatter': 'django.server',
+        # },
+        # 'mail_admins': {
+        #     'level': 'ERROR',
+        #     'filters': ['require_debug_false'],
+        #     'class': 'django.utils.log.AdminEmailHandler'
+        # },
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': 'errores_2.log',
+            'formatter': 'personal',
+            # 'formatter': 'django.server',
+        }        
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'file'],
+            'level': 'INFO',
+        },
+        # 'django.server': {
+        #     'handlers': ['django.server', 'file'],
+        #     'level': 'INFO',
+        #     'propagate': False,
+        # },
+    }
+}
