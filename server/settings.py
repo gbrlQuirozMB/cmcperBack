7ba1f2ec7d56f1f989752259ba9c5310efce6cc1
Gabriel Quiroz <gabriel@mb.company>
2021-08-06 09:57:09 -0500

    FIXED-> el logging no se mostraba adecuadamente

--- server/settings.py
+++ server/settings.py
@@ -247,7 +247,7 @@ LOGGING = {
     'handlers': {
         'console': {
             'level': 'INFO',
-            'filters': ['require_debug_true'],
+            # 'filters': ['require_debug_true'],
             'class': 'logging.StreamHandler',
             # 'formatter': 'django.server',
         },
