c9127dd68250545408466b93d9a0be07974ae253
Gabriel Quiroz <gabriel@mb.company>
2022-01-20 10:50:16 -0600

    bug: quitar link fijo sandbox en templates-> CMCPER-431

--- server/settings.py
+++ server/settings.py
@@ -199,7 +199,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATIC_ROOT = config('STATIC_ROOT', default="./static/")
 MEDIA_URL = config('MEDIA_URL', default="/uploads/")
 MEDIA_ROOT=config('MEDIA_ROOT', default="uploads/")
