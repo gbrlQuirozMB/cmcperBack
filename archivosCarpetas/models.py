d3abf7f0d5fd86cdea33378eb2dbc413e30c4e48
Gabriel Quiroz <gabriel@mb.company>
2022-02-17 17:47:39 -0600

    crear modelos-> CMCPER-457

--- archivosCarpetas/models.py
+++ archivosCarpetas/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from preregistro.models import Medico
+from django.core.validators import FileExtensionValidator
 
-# Create your models here.
+
+class Carpeta(models.Model):
+    creado_en = models.DateTimeField(auto_now_add=True)
+    actualizado_en = models.DateTimeField(auto_now=True)
+
+    medico = models.ForeignKey(Medico, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=400, blank=True, null=True)
+    # sub_carpeta = models.ForeignKey("Carpeta", blank=True, null=True, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'carpetas'
+        ordering = ['nombre']
+
+
+class Archivo(models.Model):
+    creado_en = models.DateTimeField(auto_now_add=True)
+    actualizado_en = models.DateTimeField(auto_now=True)
+
+    carpeta = models.ForeignKey(Carpeta, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=400, blank=True, null=True)
+    archivo = models.FileField(blank=True, null=True, validators=[FileExtensionValidator(allowed_extensions=['pdf', 'png', 'jpg', 'gif', 'jpeg'])], upload_to='archivosCarpetas')
+
+    class Meta:
+        db_table = 'archivos'
+        ordering = ['nombre']
--- archivosCarpetas/tests.py
+++ archivosCarpetas/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
+from .models import *
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+
+
+def configDB():
+    medico1 = Medico.objects.create(
+        nombre='gabriel', apPaterno='quiroz', apMaterno='olvera', rfc='quog??0406', curp='curp3', fechaNac='2020-09-09', pais='pais3', estado='estado3', ciudad='ciudad3',
+        deleMuni='deleMuni3', colonia='colonia', calle='calle3', cp='cp3', numExterior='numExterior3', rfcFacturacion='rfcFacturacion3', cedProfesional='cedProfesional3',
+        cedEspecialidad='cedEspecialidad3', cedCirugiaGral='cedCirugiaGral3', hospitalResi='hospitalResi3', telJefEnse='telJefEnse3', fechaInicioResi='1999-06-06', fechaFinResi='2000-07-07',
+        telCelular='telCelular3', telParticular='telParticular3', email='gabriel@mb.company', numRegistro=333, aceptado=True, telConsultorio='telConsultorio3', sexo='M',
+        anioCertificacion=2022, isConsejero=True, isProfesor=False, isCertificado=False, estudioExtranjero=False, isExtranjero=False)
+    medico2 = Medico.objects.create(
+        nombre='elianid', apPaterno='tolentino', apMaterno='tolentino', rfc='quog??0406', curp='curp1', fechaNac='2020-09-09', pais='pais1', estado='estado1', ciudad='ciudad1',
+        deleMuni='deleMuni1', colonia='colonia', calle='calle1', cp='cp1', numExterior='numExterior1', rfcFacturacion='rfcFacturacion1', cedProfesional='cedProfesional1',
+        cedEspecialidad='cedEspecialidad1', cedCirugiaGral='cedCirugiaGral1', hospitalResi='hospitalResi1', telJefEnse='telJefEnse1', fechaInicioResi='1999-06-06', fechaFinResi='2000-07-07',
+        telCelular='telCelular1', telParticular='telParticular1', email='elianid@mb.company', numRegistro=111, aceptado=True, telConsultorio='telConsultorio1', sexo='F',
+        anioCertificacion=2022, isConsejero=True, isProfesor=False, isCertificado=True, estudioExtranjero=False, isExtranjero=False)
+    
+    carpeta1 = Carpeta.objects.create(medico=medico1, nombre='carpeta1')
+    carpeta2 = Carpeta.objects.create(medico=medico1, nombre='carpeta2')
+    carpeta3 = Carpeta.objects.create(medico=medico2, nombre='carpeta3')
+    
+    Archivo.objects.create(carpeta=carpeta1, nombre='sexoso.jpg', archivo='sexoso.jpg')
+    Archivo.objects.create(carpeta=carpeta1, nombre='fotos calientes.jpg', archivo='fot-Hot.jpg')
+    Archivo.objects.create(carpeta=carpeta2, nombre='gatitas sexys.jpg', archivo='pussy.jpg')
+    Archivo.objects.create(carpeta=carpeta3, nombre='juguetes sexuales.jpg', archivo='toys.jpg')
+    Archivo.objects.create(carpeta=carpeta2, nombre='no se algo de sexo.jpg', archivo='unknow-sex.jpg')
+    
+
+
+# python manage.py test archivosCarpetas.tests.BaseDatosTest
+class BaseDatosTest(APITestCase):
+    def setUp(self):
+        configDB()
+        self.user = User.objects.create_user(username='gabriel', is_staff=True)  # IsAuthenticated
+
+    def test(self):
+        self.client.force_authenticate(user=self.user)
+
+        datos = Archivo.objects.all()
+        for dato in datos:
+            print(f'--->>>id: {dato.id} - carpeta: {dato.carpeta.nombre} - medico: {dato.carpeta.medico.nombre} - nombre: {dato.nombre} - archivo: {dato.archivo}')
+
